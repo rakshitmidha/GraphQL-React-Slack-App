ef843a9e63d46ea0b41f884e2b585427fe48e921
rakshitmidha <rmidha@collaboration-factory.de>
2018-05-26 17:59:36 +0200

    Initialize config

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  scapholdUrl: 'us-west-2.api.scaphold.io/graphql/merciful-property',
+  auth0ClientId: 'KdQRmL2r0F3IEt104SUCp5dD1Ftl3OM_',
+  auth0Domain: 'rakshit-slackr-app.eu.auth0.com',
+}
+
+export default config;
